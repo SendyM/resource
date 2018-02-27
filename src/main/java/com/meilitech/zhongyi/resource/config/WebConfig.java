8c8505ed957a95ec0d3ca2c1f8dca2b7f40dcee0
Sendy <daishihua@sanjiang365.com>
2018-02-27 11:09:41 +0800

    规范代码

--- src/main/java/com/meilitech/zhongyi/resource/config/WebConfig.java
+++ src/main/java/com/meilitech/zhongyi/resource/config/WebConfig.java
@@ -1,21 +1,17 @@
 package com.meilitech.zhongyi.resource.config;
 
-
-
-
-        import java.io.IOException;
-
-
-        import com.fasterxml.jackson.databind.*;
-        import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.context.annotation.Primary;
-        import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-
-        import com.fasterxml.jackson.core.JsonGenerator;
-        import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import java.io.IOException;
 
 
 @Configuration
