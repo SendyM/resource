009c7b7136b4f691378a25fbf7536eb24dfa0723
Sendy <daishihua@sanjiang365.com>
2018-02-27 10:46:46 +0800

    Redis更新为远程地址

--- src/main/java/com/meilitech/zhongyi/resource/ResourceApplication.java
+++ src/main/java/com/meilitech/zhongyi/resource/ResourceApplication.java
@@ -3,6 +3,9 @@ package com.meilitech.zhongyi.resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Sendy
+ */
 @SpringBootApplication
 public class ResourceApplication {
     public static void main(String[] args) {
